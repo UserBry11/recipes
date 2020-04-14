fc7adffc1a1675d2900b4d89a337189b92fe8ea3
Bryan Fernandez <bryfern710@gmail.com>
2020-04-13 23:53:38 -0500

    heroku deployment

--- recipe_box/settings.py
+++ recipe_box/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = '&6krf%=v2(si!!xx*2og49$ue(^ntbs_gc+#oubctlxs*5lh&u'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['recipe-box-1234.herokuapp.com', 'localhost']
 
