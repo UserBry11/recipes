fecd6f68721a121b376675af7821b38c22c98f06
Bryan Fernandez <bryfern710@gmail.com>
2020-04-13 23:38:17 -0500

    first heroku push

--- recipe_box/settings.py
+++ recipe_box/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = '&6krf%=v2(si!!xx*2og49$ue(^ntbs_gc+#oubctlxs*5lh&u'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = ['*']
+ALLOWED_HOSTS = ['recipe-box-1234.herokuapp.com', 'localhost']
 
 
 # Application definition
